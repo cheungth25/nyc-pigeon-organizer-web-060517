2d57463636a7b76996004d5a0779ff55e357aa49
Thomas Cheung <cheungth25@gmail.com>
2017-05-30 21:17:16 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  new_hash = {}
+  data.each { |key1, value1|
+    value1.each { |key2, value2|
+      value2.each { |value3|
+      new_hash[value3] = {} if new_hash[value3] == nil
+      new_hash[value3][key1] = [] if new_hash[value3][key1] == nil
+      new_hash[value3][key1] << key2.to_s
+      }
+    }
+  }
+  return new_hash
 end
